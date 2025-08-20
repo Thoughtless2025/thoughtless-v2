226ceac81e4bdcbdc06580a26081108b457c3d4c
Thoughtless2025 <thoughtless2025@gmail.com>
2025-08-20 19:33:29 +0000

    fix: add firebase config to frontend

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBKMlvdvDD3ny22ILHSmap2utFz5ORk6Sw",
+  authDomain: "thoughtless-v2.firebaseapp.com",
+  projectId: "thoughtless-v2",
+  storageBucket: "thoughtless-v2.appspot.com",
+  messagingSenderId: "549296244347",
+  appId: "1:549296244347:web:fdc21f5202baf98f78c4de",
+  measurementId: "G-M0KHH5Z6YY"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
